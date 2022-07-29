eceaf19e3041631d3cdcb30e7b00e54e578f10a0
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-07-29 16:17:50 -0300

    feat: crud

--- db/migrate/20220729003638_create_requests.rb
+++ db/migrate/20220729003638_create_requests.rb
@@ -2,6 +2,7 @@ class CreateRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :requests do |t|
       t.references :user, null: false, foreign_key: true
+      t.integer :status
 
       t.timestamps
     end

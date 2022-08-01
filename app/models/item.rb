66b78b3f67acbd7510735eab47a0b2f2732cb18c
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-08-01 15:33:45 -0300

    feat: crud

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :shop
+  has_many :request_items
+  has_many :requests, through: :request_items
+
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :user
   has_many :request_items
+  has_many :items, through: :request_items
 end
